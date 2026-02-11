ad7472f8dbbc9af274be90be1e7bb70097a8f0cd
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:53:10 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -22,10 +22,10 @@ EOT
     resource_group_name           = string
     sub_domain                    = string
     display_name                  = optional(string)
-    public_network_access_enabled = optional(bool, true)
-    sku                           = optional(string, "ST1")
+    public_network_access_enabled = optional(bool)   # Default: true
+    sku                           = optional(string) # Default: "ST1"
     tags                          = optional(map(string))
-    template                      = optional(string, "iotc-pnp-preview@1.0.0")
+    template                      = optional(string) # Default: "iotc-pnp-preview@1.0.0"
     identity = optional(object({
       type = string
     }))
